ae1fe25f205968384acfff17f7ab278f5a4dd2b5
Peter Hegedűs <hegedus.peter@webdesign.co.hu>
2026-06-26 19:47:30 +0200

    first commit

---
+++ setup.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * -------------------------------------------------------------------------
+ * CronGuard plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of CronGuard.
+ *
+ * CronGuard is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CronGuard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CronGuard. If not, see <https://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2026 Buwl.
+ * @author    Peter Hegedűs
+ * @license   GPL-2.0-or-later
+ * @link      https://github.com/buwl-hu/cronguard
+ * -------------------------------------------------------------------------
+ */
+function plugin_version_cronguard(): array
+{
+    return [];
+}
